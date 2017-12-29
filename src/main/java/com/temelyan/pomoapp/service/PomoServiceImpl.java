8422741a530203bc30567cba68e0aba8148188a2
Artyom Emelyanenko <tema.emelyan@gmail.com>
2017-12-29 19:15:49 +1000

    added controller, and service implementations

---
+++ src/main/java/com/temelyan/pomoapp/service/PomoServiceImpl.java
@@ -0,0 +1,7 @@
+package com.temelyan.pomoapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PomoServiceImpl implements PomoService {
+}
---
+++ src/main/java/com/temelyan/pomoapp/web/RootController.java
@@ -0,0 +1,27 @@
+package com.temelyan.pomoapp.web;
+
+import com.temelyan.pomoapp.service.PomoService;
+import com.temelyan.pomoapp.service.UserSevice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserSevice userSevice;
+
+    @Autowired
+    private PomoService pomoService;
+
+    @GetMapping("/")
+    public String root() {
+        logger.info("redirect from root to index.jsp");
+        return "index";
+    }
+}
