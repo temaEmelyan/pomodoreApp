b84b2c7b4b0ffd2fe07f1b2b8200d360410d8975
Artyom Emelyanenko <tema.emelyan@gmail.com>
2018-04-09 20:23:17 +1000

    configured log file

--- src/main/java/com/temelyan/pomoapp/WebApplication.java
+++ src/main/java/com/temelyan/pomoapp/WebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApplication {
 
     public static void main(String[] args) {
+        System.out.println("Env home folder is: " + System.getenv("HOME"));
         new SpringApplication(WebApplication.class).run(args);
     }
 }
