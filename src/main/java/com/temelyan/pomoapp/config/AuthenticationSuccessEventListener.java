fbb6c1f4711b2f3ab564075c394402cf0519c893
Artyom Emelyanenko <tema.emelyan@gmail.com>
2018-04-10 14:20:02 +1000

    attempt to fix session max inactive interval setup

---
+++ src/main/java/com/temelyan/pomoapp/config/AuthenticationSuccessEventListener.java
@@ -0,0 +1,34 @@
+package com.temelyan.pomoapp.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Event listener listening for any successful authentication events.
+ * onApplicationEvent method gets triggered
+ */
+@Component
+public class AuthenticationSuccessEventListener implements ApplicationListener<AuthenticationSuccessEvent> {
+    private HttpServletRequest httpRequest;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    public void setHttpRequest(HttpServletRequest httpRequest) {
+        this.httpRequest = httpRequest;
+    }
+
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent e) {
+        HttpSession session = httpRequest.getSession();
+        int interval = 60 * 60 * 24 * 7;//7 days
+        session.setMaxInactiveInterval(interval);
+        logger.info("session max inactive interval is: " + session.getMaxInactiveInterval());
+    }
+}
