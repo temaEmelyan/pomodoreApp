98fbbc02eed8a055a5b4dd36350942092fb983fb
Artyom Emelyanenko <tema.emelyan@gmail.com>
2018-04-11 22:09:07 +1000

    deleted tos

--- src/main/java/com/temelyan/pomoapp/to/ProjectToFactory.java
+++
@@ -1,9 +0,0 @@
-package com.temelyan.pomoapp.to;
-
-import com.temelyan.pomoapp.model.Project;
-
-public class ProjectToFactory {
-    public static ProjectTo fromEntity(Project project) {
-        return new ProjectTo(project.getId(), project.getName());
-    }
-}
