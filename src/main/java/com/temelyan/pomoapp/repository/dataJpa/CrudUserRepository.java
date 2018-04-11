c70969950229a88bdae5c2298fb7b5aca29ae3e1
Artyom Emelyanenko <tema.emelyan@gmail.com>
2018-04-11 22:31:00 +1000

    refactored user service and repository

--- src/main/java/com/temelyan/pomoapp/repository/dataJpa/CrudUserRepository.java
+++ src/main/java/com/temelyan/pomoapp/repository/dataJpa/CrudUserRepository.java
@@ -1,24 +1,20 @@
 package com.temelyan.pomoapp.repository.dataJpa;
 
 import com.temelyan.pomoapp.model.User;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CrudUserRepository extends JpaRepository<User, Integer> {
-    User getByEmail(String email);
+    Optional<User> getByEmail(String email);
 
+    @Override
     Optional<User> findById(Integer id);
 
     @Override
     @Transactional
     User save(User user);
 
-    @Override
-    List<User> findAll(Sort sort);
-
-    User findByResetToken(String resetToken);
+    Optional<User> findByResetToken(String resetToken);
 }
