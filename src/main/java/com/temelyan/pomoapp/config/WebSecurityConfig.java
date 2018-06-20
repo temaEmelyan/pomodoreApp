ff8d6b6e8992a1b7ea1c80685fe9637bb6466fc3
Artyom Emelyanenko <tema.emelyan@gmail.com>
2018-06-21 01:16:45 +1000

    redirecting http to https

--- src/main/java/com/temelyan/pomoapp/config/WebSecurityConfig.java
+++ src/main/java/com/temelyan/pomoapp/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,17 +12,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder encoder;
     private final UserDetailsService userDetailsService;
+    private final Environment environment;
 
     @Autowired
     public WebSecurityConfig(@Qualifier("userService") UserDetailsService userDetailsService,
-                             PasswordEncoder encoder) {
+                             PasswordEncoder encoder, Environment environment) {
         this.userDetailsService = userDetailsService;
         this.encoder = encoder;
+        this.environment = environment;
     }
 
     @Bean
@@ -46,6 +51,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureForwardUrl("/fail").permitAll()
                 .and()
                 .logout().permitAll();
+
+        Boolean requireHTTPS = false;
+        if (Arrays.asList(environment.getActiveProfiles()).contains("aws")) {
+            requireHTTPS = true;
+        }
+
+        if (requireHTTPS) {
+            http.requiresChannel().anyRequest().requiresSecure();
+        }
     }
 
     @Autowired
