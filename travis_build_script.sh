ad8e15ae69d07b247b35a236674fa84ad194522a
Artyom Emelyanenko <tema.emelyan@gmail.com>
2019-04-10 03:16:10 +1000

    fixing bash syntax in travis build file

--- travis_build_script.sh
+++ travis_build_script.sh
@@ -2,9 +2,9 @@
 set -e
 mvn package
 
-if $TRAVIS_BRANCH=="master"; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
     aws ecs update-service --cluster pomodoro-prod --service pomodoro-prod --force-new-deployment #todo create this cluster
-elif $TRAVIS_BRANCH=="develop"; then
+elif [ "$TRAVIS_BRANCH" == "develop" ]; then
     aws ecs update-service --cluster pomodoro-dev --service pomodoro-dev --force-new-deployment #todo create this cluster
 else
     aws ecs update-service --cluster pomodoro --service pomodoro --force-new-deployment #todo rename cluster to sandbox
